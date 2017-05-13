f5616cd9bbbb356cc9622fc16d086f6576e54a45
Volodymyr Gotra <vgotra@gmail.com>
2017-05-13 03:39:37 -0400

    Updated components, moved to C#7 partially, reused Roslyn, etc.

--- utilities/FileFormatDetection.Generator/Program.cs
+++ utilities/FileFormatDetection.Generator/Program.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using FileFormatDetection.Core;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxKind;
 
 namespace FileFormatDetection.Generator
 {
+    /*
+    Reused https://github.com/KirillOsenkov/RoslynQuoter
+    */
+
     public class Program
     {
         public static void Main(string[] args)
@@ -24,88 +27,174 @@ namespace FileFormatDetection.Generator
 
             foreach (var file in files)
             {
-                var fileFormats = FileFormatsLoader.LoadFromJsonFile(file);
-                GenerateCsFile(directory, fileFormats);
+                var fileFormats = file.LoadFromJsonFile();
+                GenerateCsFileRoslyn(directory, fileFormats);
             }
         }
-
-        public static void GenerateCsFile(string directory, List<FileFormat> fileFormats)
-        {
-            var type = fileFormats.First().Type;
-
-            var lines = new List<string>();
-
-            foreach (var file in fileFormats)
-            {
-                lines.Add($"public FileFormat {type}{file.Name} => FileFormats.First(x => x.Name.Equals(\"{file.Name}\", StringComparison.OrdinalIgnoreCase));");
-            }
-
-            var stringLines = lines.Aggregate((f, s) => $"{f}{Environment.NewLine}{Environment.NewLine}        {s}");
-
-            var classToWrite = $@"using System;
-using System.Linq;
-using System.Collections.Generic;
-using FileFormatDetection.Core;
-
-namespace FileFormatDetection.Signatures
-{{
-    public class {type}
-    {{
-        public List <FileFormat> FileFormats {{ get; }}
-
-        public {type}(string formatsContent)
-        {{
-            FileFormats = FileFormatsLoader.LoadFromJsonContent(formatsContent);
-        }}
-
-        {stringLines}
-    }}
-}}";
-
-            File.WriteAllText(Path.Combine(directory, type + ".cs"), classToWrite);
-        }
-
 
         public static void GenerateCsFileRoslyn(string directory, List<FileFormat> fileFormats)
         {
-            var type = fileFormats.First().Type;
+            #region Local functions
 
-            var pds = new List<MemberDeclarationSyntax>();
-
-            foreach (var file in fileFormats)
+            SyntaxList<UsingDirectiveSyntax> GenerateUsings()
             {
-                var pd = SF.PropertyDeclaration(SF.ParseTypeName("FileFormat"), $"{type}{file.Name}")
-                    .AddModifiers(SF.Token(SyntaxKind.PublicKeyword))
-                    .AddAccessorListAccessors(
-                        SF.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-                            .AddBodyStatements(SF.ParseStatement($"return FileFormats.First(x => x.Name.Equals(\"{file.Name}\", StringComparison.OrdinalIgnoreCase));"))
-                    )
-                    .NormalizeWhitespace();
+                /*
+                using System.Linq;
+                using System.Collections.Generic;
+                using FileFormatDetection.Core;
+                */
 
-                pds.Add(pd);
+                return List(
+                    new[]
+                    {
+                        UsingDirective(IdentifierName($"{nameof(System)}")),
+                        UsingDirective(QualifiedName(IdentifierName($"{nameof(System)}"),
+                            IdentifierName($"{nameof(System.Linq)}"))),
+                        UsingDirective(QualifiedName(
+                            QualifiedName(IdentifierName($"{nameof(System)}"),
+                                IdentifierName($"{nameof(System.Collections)}")),
+                            IdentifierName($"{nameof(System.Collections.Generic)}"))),
+                        UsingDirective(QualifiedName(IdentifierName($"{nameof(FileFormatDetection)}"),
+                            IdentifierName($"{nameof(Core)}")))
+                    });
             }
 
-            var classFile = SF.CompilationUnit()
-                .AddUsings(
-                    //SyntaxFactory.UsingDirective(SyntaxFactory.QualifiedName(SyntaxFactory.IdentifierName("System"), SyntaxFactory.IdentifierName("Linq")))
-                    SF.UsingDirective(SF.IdentifierName("System")).NormalizeWhitespace(),
-                    SF.UsingDirective(SF.IdentifierName("System.Linq")).NormalizeWhitespace(),
-                    SF.UsingDirective(SF.IdentifierName("System.Collections.Generic")).NormalizeWhitespace(),
-                    SF.UsingDirective(SF.IdentifierName($"{nameof(FileFormatDetection)}.{nameof(FileFormatDetection.Core)}")).NormalizeWhitespace()
-                ).NormalizeWhitespace()
-                .AddMembers(SF.NamespaceDeclaration(SF.IdentifierName($"FileFormatDetection.Signatures")).NormalizeWhitespace()
-                    .AddMembers(SF.ClassDeclaration(type).AddModifiers(SF.Token(SyntaxKind.PublicKeyword))
-                        .AddMembers(
-                            SF.PropertyDeclaration(SF.ParseTypeName("List<FileFormat>"), "FileFormats")
-                                .AddAccessorListAccessors(SF.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration).WithSemicolonToken(SF.Token(SyntaxKind.SemicolonToken)))
-                                .AddModifiers(SF.Token(SyntaxKind.PublicKeyword))
-                                .NormalizeWhitespace(),
-                            SF.ConstructorDeclaration(type).AddModifiers(SF.Token(SyntaxKind.PublicKeyword))
-                                .AddParameterListParameters(SF.Parameter(SF.Identifier("formatsContent")).WithType(SF.ParseTypeName("string")))
-                                .AddBodyStatements(SF.ParseStatement("FileFormats = FileFormatsLoader.LoadFromJsonContent(formatsContent);"))
-                        )
-                        .AddMembers(pds.ToArray()).NormalizeWhitespace()
-                    ));
+            PropertyDeclarationSyntax GenerateProperty()
+            {
+                /*
+                public List<FileFormat> FileFormats { get; }
+                */
+
+                return PropertyDeclaration(
+                        GenericName(Identifier("List"))
+                            .WithTypeArgumentList(
+                                TypeArgumentList(
+                                    SingletonSeparatedList<TypeSyntax>(IdentifierName($"{nameof(FileFormat)}")))),
+                        Identifier("FileFormats"))
+                    .WithModifiers(TokenList(Token(PublicKeyword)))
+                    .WithAccessorList(AccessorList(SingletonList(AccessorDeclaration(GetAccessorDeclaration)
+                        .WithSemicolonToken(Token(SemicolonToken)))));
+            }
+
+            ConstructorDeclarationSyntax GenerateConstructor(string formatType)
+            {
+                /*
+                public Audio(string formatsContent)
+                {
+                    FileFormats = FileFormatsLoader.LoadFromJsonContent(formatsContent);
+                }
+                */
+
+                return ConstructorDeclaration(Identifier(formatType))
+                    .WithModifiers(TokenList(Token(PublicKeyword)))
+                    .WithParameterList(
+                        ParameterList(
+                            SingletonSeparatedList(
+                                Parameter(
+                                        Identifier("formatsContent"))
+                                    .WithType(
+                                        PredefinedType(
+                                            Token(StringKeyword))))))
+                    .WithBody(
+                        Block(
+                            SingletonList<StatementSyntax>(
+                                ExpressionStatement(
+                                    AssignmentExpression(
+                                        SimpleAssignmentExpression,
+                                        IdentifierName("FileFormats"),
+                                        InvocationExpression(
+                                                MemberAccessExpression(
+                                                    SimpleMemberAccessExpression,
+                                                    IdentifierName($"{nameof(FileFormatsLoader)}"),
+                                                    IdentifierName($"{nameof(FileFormatsLoader.LoadFromJsonContent)}")))
+                                            .WithArgumentList(
+                                                ArgumentList(
+                                                    SingletonSeparatedList(
+                                                        Argument(IdentifierName(
+                                                            "formatsContent"))))))))));
+            }
+
+            PropertyDeclarationSyntax GeneratePropertyDeclarationForFormat(string formatType, string fileFormat)
+            {
+                /*
+                public FileFormat AudioMp3 => FileFormats.First(x => x.Name.Equals("Mp3", StringComparison.OrdinalIgnoreCase));
+                */
+                return PropertyDeclaration(
+                        IdentifierName($"{nameof(FileFormat)}"),
+                        Identifier($"{formatType}{fileFormat}"))
+                    .WithModifiers(
+                        TokenList(
+                            Token(PublicKeyword)))
+                    .WithExpressionBody(
+                        ArrowExpressionClause(
+                            InvocationExpression(
+                                    MemberAccessExpression(
+                                        SimpleMemberAccessExpression,
+                                        IdentifierName("FileFormats"),
+                                        IdentifierName("First")))
+                                .WithArgumentList(
+                                    ArgumentList(
+                                        SingletonSeparatedList(
+                                            Argument(
+                                                SimpleLambdaExpression(
+                                                    Parameter(Identifier("x")),
+                                                    InvocationExpression(
+                                                            MemberAccessExpression(
+                                                                SimpleMemberAccessExpression,
+                                                                MemberAccessExpression(
+                                                                    SimpleMemberAccessExpression,
+                                                                    IdentifierName(
+                                                                        "x"),
+                                                                    IdentifierName(
+                                                                        "Name")),
+                                                                IdentifierName(
+                                                                    "Equals")))
+                                                        .WithArgumentList(
+                                                            ArgumentList(
+                                                                SeparatedList<
+                                                                    ArgumentSyntax>(
+                                                                    new
+                                                                        SyntaxNodeOrToken
+                                                                        []
+                                                                        {
+                                                                            Argument(LiteralExpression(StringLiteralExpression, Literal(fileFormat))),
+                                                                            Token(CommaToken),
+                                                                            Argument(MemberAccessExpression(SimpleMemberAccessExpression, IdentifierName("StringComparison"), IdentifierName("OrdinalIgnoreCase")))
+                                                                        }))))))))))
+                    .WithSemicolonToken(
+                        Token(SemicolonToken));
+            }
+
+            #endregion
+
+            var type = fileFormats.First().Type;
+
+            var classBody = new List<MemberDeclarationSyntax>
+            {
+                GenerateProperty(),
+                GenerateConstructor(type)
+            };
+
+            foreach (var fileFormat in fileFormats)
+            {
+                classBody.Add(GeneratePropertyDeclarationForFormat(fileFormat.Type, fileFormat.Name));
+            }
+
+            var classFile = CompilationUnit()
+                .WithUsings(GenerateUsings())
+                .WithMembers(
+                    SingletonList<MemberDeclarationSyntax>(
+                        NamespaceDeclaration(QualifiedName(IdentifierName($"{nameof(FileFormatDetection)}"),
+                                IdentifierName($"{nameof(FileFormatDetection.Signatures)}")))
+                            .WithMembers(
+                                SingletonList<MemberDeclarationSyntax>(
+                                    ClassDeclaration(type)
+                                        .WithModifiers(
+                                            TokenList(
+                                                Token(PublicKeyword)))
+                                        .WithMembers(
+                                            List(classBody))))))
+                .NormalizeWhitespace();
 
             File.WriteAllText(Path.Combine(directory, type + ".cs"), classFile.ToFullString());
         }
